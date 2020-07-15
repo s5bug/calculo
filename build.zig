04ad40b346a3bb8d83e0d2101d2c5506823dbc21
Soren <soren@disroot.org>
2020-07-15 14:28:19 -0700

    Add zgl as a submodule

--- build.zig
+++ build.zig
@@ -14,6 +14,9 @@ pub fn build(b: *Builder) void {
     const exe = b.addExecutable("calculo", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    exe.addPackagePath("zgl", "lib/zgl/zgl.zig");
+
     exe.install();
 
     const run_cmd = exe.run();
