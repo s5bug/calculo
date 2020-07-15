7724d4690b0288bbd4949206b85ba52ac5157de0
Soren <soren@disroot.org>
2020-07-14 17:25:19 -0700

    Standard function for checking if something is a provider

--- src/calculo/provider.zig
+++ src/calculo/provider.zig
@@ -4,14 +4,18 @@ const puyo = @import("../puyo/puyo.zig");
 
 const state = @import("state.zig");
 
-pub const is_provider = std.meta.trait.multiTrait(.{
+const is_provider_trait = std.meta.trait.multiTrait(.{
     std.meta.trait.hasFn("init"),
     std.meta.trait.hasFn("pull"),
 });
 
-pub fn do_thing(provider: anytype) void {
-    comptime std.debug.assert(is_provider(@TypeOf(provider)));
+pub fn assert_is_provider(provider: anytype) void {
+    comptime std.debug.assert(is_provider_trait(@TypeOf(provider)));
     comptime std.debug.assert(@TypeOf(provider.init()) == void);
     comptime std.debug.assert(@TypeOf(provider.pull()) == state.GameState);
+}
+
+pub fn do_thing(provider: anytype) void {
+    assert_is_provider(provider);
     std.debug.print("{}\n", .{provider.pull()});
 }
